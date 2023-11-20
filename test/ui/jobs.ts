96d0a54d5468116374d005249d9d4bb82d134a04
Thomas-Kainos <thomas.hunt@kainos.com>
2023-11-20 13:24:24 +0000

    Reverting everything back to the last time it built

--- test/ui/jobs.ts
+++ test/ui/jobs.ts
@@ -2,86 +2,44 @@ import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
 import chrome from 'selenium-webdriver/chrome';
 
-interface CustomWebDriver extends WebDriver {
-  setTimeouts(timeouts: { implicit: number }): Promise<void>;
-}
-
-function extendWebDriver(driver: WebDriver): CustomWebDriver {
-  const customDriver = driver as CustomWebDriver;
-
-  customDriver.setTimeouts = async function (timeouts: { implicit: number }): Promise<void> {
-    await driver.manage().setTimeouts(timeouts);
-  };
-
-  return customDriver;
-}
-
-let driver: CustomWebDriver;
-
-before(async function (this: Mocha.Context) {
+describe('Landing Page', function() {
   this.timeout(100000);
 
-  try {
-    console.log('Starting WebDriver setup...');
+  let driver: WebDriver;
+
+  before(async function() {
     const options = new chrome.Options();
-    options.addArguments('--headless');
-    options.addArguments('--start-maximized');
-    driver = extendWebDriver(await new Builder().forBrowser('chrome').setChromeOptions(options).build());
-
-    console.log('WebDriver setup completed. Setting timeouts...');
-    await driver.setTimeouts({ implicit: 100000 });
-
-    console.log('Navigating to the application...');
+    options.addArguments('--headless'); 
+    options.addArguments('--window-size=1920,1080');
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
     await driver.get('http://localhost:3000');
+  });
 
-    console.log('Application navigation completed.');
-  } catch (error) {
-    console.error('Error during WebDriver setup or navigation:', error);
-    throw error;
-  }
-});
+  describe('"View Job Roles" button', function() {
+    it('should load job list when clicked', async function() {
+      const button = await driver.findElement(By.id('viewJobRolesButton'));
+      await new Promise(resolve => setTimeout(resolve, 10000));
+      await button.click();
+      
+      const jobTable = await driver.wait(until.elementLocated(By.id('jobs')), 30000);
+      expect(await jobTable.isDisplayed()).to.be.true;
+    });
+  });
 
-describe('"View Job Roles" button', function () {
-  it('should load job list when clicked', async function () {
-    this.timeout(100000); 
+  describe('"First View in Sharepoint" button', function() {
+    it('should redirect to the appropriate page when clicked', async function() {
+      const jobId = 1;
+      const button = await driver.findElement(By.id(`viewSharePointButton_${jobId}`));
+      await button.click();
 
-    const button = await driver.wait(until.elementLocated(By.id('viewJobRolesButton')), 100000);
-    await button.click();
+      await driver.wait(until.urlContains('sharepoint'), 20000);
 
+      const url = await driver.getCurrentUrl();
+      expect(url).to.include('sharepoint');
+    });
+  });
 
-    const jobTable = await driver.findElement(By.id('jobs'));
-    expect(await jobTable.isDisplayed()).to.be.true;
+  after(async function() {
+    await driver.quit();
   });
 });
-
-describe('"First View in Sharepoint" button', function () {
-  it('should redirect to the appropriate page when clicked', async function () {
-    this.timeout(100000);
-
-    const jobId = 1;
-    const button = await driver.wait(until.elementLocated(By.id(`viewSharePointButton_${jobId}`)), 100000);
-    await button.click();
-
-    await driver.wait(until.urlContains('sharepoint'));
-
-    const url = await driver.getCurrentUrl();
-    expect(url).to.include('sharepoint');
-  });
-});
-
-after(async function (this: Mocha.Context) {
-  this.timeout(100000);
-
-  if (driver) {
-    try {
-      console.log('Quitting WebDriver...');
-      await driver.quit();
-      console.log('WebDriver quit successfully.');
-    } catch (error) {
-      console.error('Error during WebDriver quit:', error);
-    }
-  } else {
-    console.warn('WebDriver is undefined. Quit skipped.');
-  }
-});
-
