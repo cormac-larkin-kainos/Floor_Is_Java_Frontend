a67cd6ee502b80b0e9bd778df723c931ca2a5c7f
Thomas-Kainos <thomas.hunt@kainos.com>
2023-11-20 18:17:35 +0000

    reverting?

--- test/ui/jobs.ts
+++ test/ui/jobs.ts
@@ -1,49 +1,43 @@
-import { Builder, By, Capabilities, logging, until, WebDriver } from 'selenium-webdriver';
+import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
-import { Options } from 'selenium-webdriver/chrome';
+import chrome from 'selenium-webdriver/chrome';
 
-describe('Landing Page', function() {
+describe('Job Roles Tests', function() {
   this.timeout(100000);
 
   let driver: WebDriver;
 
   before(async function() {
-    const options = new Options();
-    options.headless().windowSize({
-      height: 1080,
-      width: 1920,
-    });
-
-    driver = await new Builder().withCapabilities(Capabilities.chrome()).setChromeOptions(options).build();
+    const options = new chrome.Options();
+    options.addArguments('--headless'); 
+    options.addArguments('--window-size=1920,1080');
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+    await driver.get('http://localhost:3000');
   });
 
-  describe('"View Job Roles" button', function() {
-    it('should load job list when clicked', async function() {
-      await driver.get('http://localhost:3000');
-      const button = await driver.findElement(By.id('viewJobRolesButton'));
-      await new Promise(resolve => setTimeout(resolve, 10000));
-      await button.click();
-      
-      const jobTable = await driver.wait(until.elementLocated(By.id('jobs')), 30000);
-      expect(await jobTable.isDisplayed()).to.be.true;
-    });
+  it('should load job list and redirect to Sharepoint page when clicked', async function () {
+    const viewJobRolesButton = await driver.findElement(By.id('viewJobRolesButton'));
+    await driver.wait(until.elementIsVisible(viewJobRolesButton), 10000);
+    await viewJobRolesButton.click();
+
+
+    const jobTable = await driver.wait(until.elementLocated(By.id('jobs')), 30000);
+    await driver.wait(until.elementIsVisible(jobTable), 10000);
+
+    expect(await jobTable.isDisplayed()).to.be.true;
+
+    const jobId = 1;
+    const viewSharePointButton = await driver.findElement(By.id(`viewSharePointButton_${jobId}`));
+    await driver.wait(until.elementIsVisible(viewSharePointButton), 10000);
+    await viewSharePointButton.click();
+
+    await driver.wait(until.urlContains('sharepoint'), 20000);
+
+    const url = await driver.getCurrentUrl();
+    expect(url).to.include('sharepoint');
   });
 
-  describe('"First View in Sharepoint" button', function() {
-    it('should redirect to the appropriate page when clicked', async function() {
-      await driver.get('http://localhost:3000');
-      const jobId = 1;
-      const button = await driver.findElement(By.id(`viewSharePointButton_${jobId}`));
-      await button.click();
-
-      await driver.wait(until.urlContains('sharepoint'), 20000);
-
-      const url = await driver.getCurrentUrl();
-      expect(url).to.include('sharepoint');
-    });
-  });
-
-  after(async function() {
+  after(async function () {
     await driver.quit();
   });
 });
