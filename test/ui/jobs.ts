bffda90fa2c28f814dbdb1aae332e069f6e5b543
Thomas-Kainos <thomas.hunt@kainos.com>
2023-11-20 12:24:51 +0000

    custom driveR

--- test/ui/jobs.ts
+++ test/ui/jobs.ts
@@ -2,49 +2,67 @@ import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
 import chrome from 'selenium-webdriver/chrome';
 
-describe('Landing Page', function() {
-  this.timeout(100000);
+interface CustomWebDriver extends WebDriver {
+  setTimeouts(timeouts: { implicit: number }): Promise<void>;
+}
 
-  let driver: WebDriver;
+function extendWebDriver(driver: WebDriver): CustomWebDriver {
+  const customDriver = driver as CustomWebDriver;
 
-  before(async function() {
-    try {
-      const options = new chrome.Options();
-      options.addArguments('--headless'); 
-      options.addArguments('--start-maximized');
-      driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-      await driver.get('http://localhost:3000');
-    } catch (error) {
-      console.error('Error during WebDriver setup or navigation:', error);
-      throw error;
-    }
-  });  
+  customDriver.setTimeouts = async function (timeouts: { implicit: number }): Promise<void> {
+    await driver.manage().setTimeouts(timeouts);
+  };
 
-  describe('"View Job Roles" button', function() {
-    it('should load job list when clicked', async function() {
-      const button = await driver.findElement(By.id('viewJobRolesButton'));
-      await new Promise(resolve => setTimeout(resolve, 15000));
-      await button.click();
-      
-      const jobTable = await driver.wait(until.elementLocated(By.id('jobs')), 30000);
-      expect(await jobTable.isDisplayed()).to.be.true;
-    });
+  return customDriver;
+}
+
+let driver: CustomWebDriver;
+
+before(async function () {
+  try {
+    const options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--start-maximized');
+    driver = extendWebDriver(await new Builder().forBrowser('chrome').setChromeOptions(options).build());
+
+    await driver.setTimeouts({ implicit: 10000 });
+
+    await driver.get('http://localhost:3000');
+  } catch (error) {
+    console.error('Error during WebDriver setup or navigation:', error);
+    throw error;
+  }
+});
+
+describe('"View Job Roles" button', function () {
+  it('should load job list when clicked', async function () {
+
+    await driver.setTimeouts({ implicit: 15000 });
+
+    const button = await driver.findElement(By.id('viewJobRolesButton'));
+    await button.click();
+
+    const jobTable = await driver.findElement(By.id('jobs'));
+    expect(await jobTable.isDisplayed()).to.be.true;
   });
+});
 
-  describe('"First View in Sharepoint" button', function() {
-    it('should redirect to the appropriate page when clicked', async function() {
-      const jobId = 1;
-      const button = await driver.findElement(By.id(`viewSharePointButton_${jobId}`));
-      await button.click();
+describe('"First View in Sharepoint" button', function () {
+  it('should redirect to the appropriate page when clicked', async function () {
 
-      await driver.wait(until.urlContains('sharepoint'), 20000);
+    await driver.setTimeouts({ implicit: 20000 });
 
-      const url = await driver.getCurrentUrl();
-      expect(url).to.include('sharepoint');
-    });
+    const jobId = 1;
+    const button = await driver.findElement(By.id(`viewSharePointButton_${jobId}`));
+    await button.click();
+
+    await driver.wait(until.urlContains('sharepoint'));
+
+    const url = await driver.getCurrentUrl();
+    expect(url).to.include('sharepoint');
   });
+});
 
-  after(async function() {
-    await driver.quit();
-  });
+after(async function () {
+  await driver.quit();
 });
