90dcdf8140e0ac35a32a94d6de540b4c8ae1f01b
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-11-22 12:01:00 +0000

    Delete stuff

---
+++ test/ui/DeleteTests.ts
@@ -0,0 +1,70 @@
+import { Builder, By, Capabilities, until, WebDriver } from 'selenium-webdriver';
+import { expect } from 'chai';
+import { Options } from 'selenium-webdriver/chrome';
+
+describe('Delete Tests', function() {
+  this.timeout(100000);
+
+  let driver: WebDriver;
+
+  before(async function() {
+    const options = new Options();
+    options.headless().windowSize({
+      height: 1080,
+      width: 1920,
+    });
+    driver = await new Builder().withCapabilities(Capabilities.chrome()).setChromeOptions(options).build();
+    await driver.get('http://localhost:3000');
+  });
+
+  describe('Login with valid credentials', function () {
+    it('should redirect to the correct page', async function () {
+      const usernameInput = await driver.findElement(By.id('username'));
+      await usernameInput.sendKeys(process.env.TEST_VALID_ADMIN_USERNAME);
+      const passwordInput = await driver.findElement(By.id('password'));
+      await passwordInput.sendKeys(process.env.TEST_VALID_ADMIN_PASSWORD);
+      const loginButton = await driver.findElement(By.id('loginSubmit'));
+      await loginButton.click();
+  
+      const redirectedPage = await driver.wait(
+        until.urlContains('jobs'), 
+        10000
+      );
+  
+      expect(redirectedPage).to.be.true;
+    });
+  });
+  
+
+  describe('"Delete Job Roles" button', function() {
+    it('should load job selector when clicked', async function() {
+      await driver.get('http://localhost:3000');
+      const button = await driver.findElement(By.id('deleteJobRolesButton'));
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await button.click();
+      
+      const jobSelectForm = await driver.wait(until.elementLocated(By.id('jobSelectForm')), 30000);
+      expect(await jobSelectForm.isDisplayed()).to.be.true;
+    });
+  });
+
+  describe('"Select First item in dropdown" button', function() {
+    it('should redirect to the appropriate page when selected and delete button clicked', async function() {
+      const jobId = 1;
+
+      const element = await driver.findElement(By.id("1"))
+      await element.click();
+      const button = await driver.findElement(By.id(`deleteRequestSubmit`));
+      await button.click();
+
+      const url = await driver.getCurrentUrl();
+      expect(url).to.include('1');
+    });
+  });
+
+  
+
+after(async function() {
+  await driver.quit();
+});
+});
--- test/unit/service/JobServiceTests.ts
+++ test/unit/service/JobServiceTests.ts
@@ -35,6 +35,7 @@ describe('JobService', function () {
 
       expect(results[0]).to.deep.equal(job);
     });
+    
     it('should throw an exception when a 500 error is returned from axios', async () => {
       const mock = new MockAdapter(axios);
 
@@ -53,4 +54,41 @@ describe('JobService', function () {
       expect(error).to.equal('Could not get jobs');
     });
   });
+
+  describe("delete job",function() {
+    it("Should return true when attempting to delete valid id",async () => {
+      const mock = new MockAdapter(axios);
+      const API_URL = process.env.API_URL + "jobs/" + -1;
+
+      mock.onDelete(API_URL).reply(200)
+
+      let success = await jobService.deleteJob(-1);
+
+      expect(success).to.be.true;
+    });
+
+    it("Should return false when attempting to delete an id that doesn't exist", async () => {
+      const mock = new MockAdapter(axios);
+      const API_URL = process.env.API_URL + "jobs/" + -1;
+
+      mock.onDelete(API_URL).reply(404);
+
+      let success = await jobService.deleteJob(-1);
+
+      expect(success).to.be.false;
+    })
+
+    it("Should return false if a server error occurs", async () => {
+      const mock = new MockAdapter(axios);
+      const API_URL = process.env.API_URL + "jobs/" + -1;
+
+      mock.onDelete(API_URL).reply(500);
+
+      let success = await jobService.deleteJob(-1);
+
+      expect(success).to.be.false;
+    });
+
+
+  })
 });
