38619968fdf585f8511565e7e23576ca20d69b27
Ciara Ni Shiochru <ciarasugrue@gmail.com>
2023-11-22 15:14:43 +0000

    unit tests

--- controller/ResponsibilityController.ts
+++ controller/ResponsibilityController.ts
@@ -1,12 +1,14 @@
 import { Application, Request, Response } from 'express';
 import { Responsibility } from '../model/Responsibility';
 import ResponsibilityService from '../service/ResponsibilityService';
+import roleAccess from '../middleware/auth';
+import UserRole from '../model/UserRole';
 
 module.exports = function(app: Application){
 
   const responsibilityservice: ResponsibilityService = new ResponsibilityService();
 
-  app.get('/jobs/{jobId}/responsibilities', async (req: Request, res: Response) => {
+  app.get('/jobs/{jobId}/responsibilities', roleAccess([UserRole.User, UserRole.Admin]), async (req: Request, res: Response) => {
        
     let responsibilities: Responsibility[] =  [];
 
