f90bdfd7536c8934d2efa061c80684af5aaddc31
Jonny Evans <jonathan-evans99@hotmail.com>
2023-11-23 10:19:51 +0000

    Merge conflicts not here

--- controller/JobController.ts
+++ controller/JobController.ts
@@ -4,16 +4,12 @@ import JobService from '../service/JobService';
 import CapabilityService from '../service/CapabilityService';
 import roleAccess from '../middleware/auth';
 import UserRole from '../model/UserRole';
-<<<<<<< HEAD
 import { Capability } from '../model/Capability';
 import JobBandService from '../service/JobBandService';
 import { JobBand } from '../model/JobBand';
 import { JobRequest } from '../model/JobRequest';
 import getTokenRole from '../utils/getTokenRole';
 
-=======
-import getTokenRole from '../utils/getTokenRole';
->>>>>>> 575c94f5a3821388375b734d9ad2508b5ff31748
 
 module.exports = function(app: Application){
 
@@ -30,23 +26,15 @@ module.exports = function(app: Application){
     try {
       jobs = await jobservice.getAllJobs();
       res.render('view-all-jobs', {
-<<<<<<< HEAD
         role: getTokenRole(req.session.token),
         jobs: jobs, 
         token: req.session.token});
-=======
-        jobs: jobs,
-        token: req.session.token,
-        role: getTokenRole(req.session.token)
-      });
->>>>>>> 575c94f5a3821388375b734d9ad2508b5ff31748
     } catch (error) {
       console.error(error);
     }
     
   });
 
-<<<<<<< HEAD
   app.get('/add-job', roleAccess([UserRole.Admin]) ,async (req: Request, res: Response) => {
   
     let capabilities: Capability[] =  [];
@@ -54,9 +42,6 @@ module.exports = function(app: Application){
     try{
       capabilities = await capabilityservice.getCapabilities();
       jobBands = await jobbandservice.getJobBands();
-
-      console.log(capabilities);
-      console.log(jobBands);
 
       res.render('add-job', { 
         token: req.session.token,
@@ -73,39 +58,6 @@ module.exports = function(app: Application){
 
     // Is this correctly returning the new Job ID?
     const data: JobRequest = req.body;
-=======
-  app.get('/add-job',roleAccess([UserRole.Admin]), async (req: Request, res: Response) => {
-
-    const jobBands = [
-      { value: 'engineering', label: 'Engineering' },
-      { value: 'platforms', label: 'Platforms' },
-      { value: 'data&AI', label: 'Data & AI' },
-      { value: 'qualityAssurance', label: 'Quality Assurance' },
-      { value: 'cyberSecurity', label: 'Cyber Security' },
-    ];
-
-    const jobCapabilities = [
-      { value: 'engineering', label: 'Apprentice' },
-      { value: 'engineering', label: 'Trainee' },
-      { value: 'engineering', label: 'Associate' },
-      { value: 'engineering', label: 'Senior Associate' },
-      { value: 'engineering', label: 'Consultant' },
-      { value: 'engineering', label: 'Manager' },
-      { value: 'engineering', label: 'Principle' },
-      { value: 'engineering', label: 'Leadership Community' },
-    ];
-  
-    res.render('add-job', { 
-      jobBands:jobBands, 
-      jobCapabilities: jobCapabilities
-    });
-    
-  });
-
-  app.post('/add-job',roleAccess([UserRole.Admin]), async (req: Request, res: Response) => {
-    const data: Job = req.body;
-    let id: number;
->>>>>>> 575c94f5a3821388375b734d9ad2508b5ff31748
     try {
       await jobservice.addJob(data);
 
